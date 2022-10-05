d8983bec8e6f6d097607259bf37520b660e89a3f
Ahmed Anwer Saber <99138670+Ahmedanwersaber@users.noreply.github.com>
2022-10-05 21:54:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ menubar.onclick = () =>{
     menubar.classList.toggle('fa-times');
     mynav.classList.toggle('active');
 }
+
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#menu-bars').onclick=()=>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+
+}
+
+// remove menu and search on scroll 
+window.onscroll =()=>{
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+
+let header=document.querySelector('header');
+window.addEventListener('scroll',() =>{
+    header.classList.toggle('active',window.scrollY > 0)
+});
